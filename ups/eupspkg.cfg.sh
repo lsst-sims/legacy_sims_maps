2ce1577a6e4ba2cb82904b2ac6837c7135f92b30
danielsf <scott.f.daniel@gmail.com>
2015-03-10 09:37:09 -0700

    get the SFD dust map fits files from Simon's home directory on lsst-web, rather than Schlegel's home directory at Princeton (which no longer seems to exist)

--- ups/eupspkg.cfg.sh
+++ ups/eupspkg.cfg.sh
@@ -2,8 +2,8 @@ prep()
 {
     mkdir DustMaps
     cd DustMaps
-    curl -O http://www.astro.princeton.edu/~schlegel/dust/dustpub/maps/SFD_dust_4096_ngp.fits
-    curl -O http://www.astro.princeton.edu/~schlegel/dust/dustpub/maps/SFD_dust_4096_sgp.fits
+    curl -O http://lsst-web.ncsa.illinois.edu/~krughoff/data/dust_maps/SFD_dust_4096_ngp.fits
+    curl -O http://lsst-web.ncsa.illinois.edu/~krughoff/data/dust_maps/SFD_dust_4096_sgp.fits
     curl -O http://lsst-web.ncsa.illinois.edu/~krughoff/data/dust_maps/dust_nside_2.npz
     curl -O http://lsst-web.ncsa.illinois.edu/~krughoff/data/dust_maps/dust_nside_4.npz
     curl -O http://lsst-web.ncsa.illinois.edu/~krughoff/data/dust_maps/dust_nside_8.npz
