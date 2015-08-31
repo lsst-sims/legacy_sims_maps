f5e0a44523a8ded5dd9ae3d42215e4c69b674759
Peter Yoachim <yoachim@uw.edu>
2015-08-31 11:35:31 -0700

    fix typos

--- ups/eupspkg.cfg.sh
+++ ups/eupspkg.cfg.sh
@@ -2,16 +2,16 @@ prep()
 {
     mkdir DustMaps
     cd DustMaps
-    curl -O http://lsst-web.ncsa.illinois.edu/sim-data/DustMapsSFD_dust_4096_ngp.fits
-    curl -O http://lsst-web.ncsa.illinois.edu/sim-data/DustMapsSFD_dust_4096_sgp.fits
-    curl -O http://lsst-web.ncsa.illinois.edu/sim-data/DustMapsdust_nside_2.npz
-    curl -O http://lsst-web.ncsa.illinois.edu/sim-data/DustMapsdust_nside_4.npz
-    curl -O http://lsst-web.ncsa.illinois.edu/sim-data/DustMapsdust_nside_8.npz
-    curl -O http://lsst-web.ncsa.illinois.edu/sim-data/DustMapsdust_nside_16.npz
-    curl -O http://lsst-web.ncsa.illinois.edu/sim-data/DustMapsdust_nside_32.npz
-    curl -O http://lsst-web.ncsa.illinois.edu/sim-data/DustMapsdust_nside_64.npz
-    curl -O http://lsst-web.ncsa.illinois.edu/sim-data/DustMapsdust_nside_128.npz
-    curl -O http://lsst-web.ncsa.illinois.edu/sim-data/DustMapsdust_nside_256.npz
-    curl -O http://lsst-web.ncsa.illinois.edu/sim-data/DustMapsdust_nside_512.npz
-    curl -O http://lsst-web.ncsa.illinois.edu/sim-data/DustMapsdust_nside_1024.npz
+    curl -O http://lsst-web.ncsa.illinois.edu/sim-data/DustMaps/SFD_dust_4096_ngp.fits
+    curl -O http://lsst-web.ncsa.illinois.edu/sim-data/DustMaps/SFD_dust_4096_sgp.fits
+    curl -O http://lsst-web.ncsa.illinois.edu/sim-data/DustMaps/dust_nside_2.npz
+    curl -O http://lsst-web.ncsa.illinois.edu/sim-data/DustMaps/dust_nside_4.npz
+    curl -O http://lsst-web.ncsa.illinois.edu/sim-data/DustMaps/dust_nside_8.npz
+    curl -O http://lsst-web.ncsa.illinois.edu/sim-data/DustMaps/dust_nside_16.npz
+    curl -O http://lsst-web.ncsa.illinois.edu/sim-data/DustMaps/dust_nside_32.npz
+    curl -O http://lsst-web.ncsa.illinois.edu/sim-data/DustMaps/dust_nside_64.npz
+    curl -O http://lsst-web.ncsa.illinois.edu/sim-data/DustMaps/dust_nside_128.npz
+    curl -O http://lsst-web.ncsa.illinois.edu/sim-data/DustMaps/dust_nside_256.npz
+    curl -O http://lsst-web.ncsa.illinois.edu/sim-data/DustMaps/dust_nside_512.npz
+    curl -O http://lsst-web.ncsa.illinois.edu/sim-data/DustMaps/dust_nside_1024.npz
 }
