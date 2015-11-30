ed19211ebc25ac7e353158f48e89eb42fd6eb069
Peter Yoachim <yoachim@uw.edu>
2015-11-30 10:24:11 -0800

    add stellar maps

--- ups/eupspkg.cfg.sh
+++ ups/eupspkg.cfg.sh
@@ -1,6 +1,6 @@
 prep()
 {
-    mkdir DustMaps
+    mkdir -p DustMaps
     cd DustMaps
     curl -O http://lsst-web.ncsa.illinois.edu/sim-data/DustMaps/SFD_dust_4096_ngp.fits
     curl -O http://lsst-web.ncsa.illinois.edu/sim-data/DustMaps/SFD_dust_4096_sgp.fits
@@ -14,4 +14,8 @@ prep()
     curl -O http://lsst-web.ncsa.illinois.edu/sim-data/DustMaps/dust_nside_256.npz
     curl -O http://lsst-web.ncsa.illinois.edu/sim-data/DustMaps/dust_nside_512.npz
     curl -O http://lsst-web.ncsa.illinois.edu/sim-data/DustMaps/dust_nside_1024.npz
+    cd ..
+    mkdir -p StarMaps
+    cd StarMaps
+    curl -O http://lsst-web.ncsa.illinois.edu/sim-data/StarMaps/starDensity_nside_64.npz
 }
