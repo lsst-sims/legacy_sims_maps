0801ce478cb48027b76aff3eb6088cada6ae180f
Kian-Tat Lim <ktl@slac.stanford.edu>
2020-06-20 12:55:26 +0000

    Update NCSA web server URLs and handle redirects.

--- ups/eupspkg.cfg.sh
+++ ups/eupspkg.cfg.sh
@@ -2,17 +2,17 @@ install(){
     default_install
     cd $PREFIX
     cd DustMaps
-    curl -O "https://lsst-web.ncsa.illinois.edu/sim-data/DustMaps/dust_maps_161213.tar.gz"
+    curl -LO "https://lsst.ncsa.illinois.edu/sim-data/DustMaps/dust_maps_161213.tar.gz"
     tar zxvf dust_maps_161213.tar.gz
     rm dust_maps_161213.tar.gz
     cd ..
     cd StarMaps
-    curl -O "https://lsst-web.ncsa.illinois.edu/sim-data/StarMaps/star_maps_161213.tar.gz"
+    curl -LO "https://lsst.ncsa.illinois.edu/sim-data/StarMaps/star_maps_161213.tar.gz"
     tar zxvf star_maps_161213.tar.gz
     rm star_maps_161213.tar.gz
     cd ..
     cd TriMaps
-    curl -O "https://lsst-web.ncsa.illinois.edu/sim-data/TriMaps/trilegal_maps.tar.gz"
+    curl -LO "https://lsst.ncsa.illinois.edu/sim-data/TriMaps/trilegal_maps.tar.gz"
     tar zxvf trilegal_maps.tar.gz
     rm trilegal_maps.tar.gz
     cd ..
